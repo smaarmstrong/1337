4e724c8f81208ca594adf9581630361dbc90edda
smaarmstrong <seanmaarmstrong@gmail.com>
2024-11-05 17:51:23 +0000

    linear time demo in js

--- javascript/1bigo.js
+++ javascript/1bigo.js
@@ -6,3 +6,13 @@ const index = 2;
 
 // O(1) constant time
 const fruit = fruits[index];
+
+// O(n) linear time
+function printLinear(n) {
+    for (let i = 0; i < n; i++) {
+	console.log(i);
+    }
+}
+
+const linearTest = 5;
+printLinear(linearTest);
