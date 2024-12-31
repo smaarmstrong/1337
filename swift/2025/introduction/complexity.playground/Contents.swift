7e4c658ee5341828b05f8e183cdfd2fee85c1931
smaarmstrong <108431423+smaarmstrong@users.noreply.github.com>
2024-12-31 11:07:47 +0000

    notes on linear time

--- swift/2025/introduction/complexity.playground/Contents.swift
+++ swift/2025/introduction/complexity.playground/Contents.swift
@@ -22,3 +22,25 @@ func checkFirstName(names: [String]) {
 }
 
 // keep working on the above cards before doing anything else.
+
+// anki card 5
+// what is O(n) also known as?
+// linear time.
+
+// anki card 6
+// why is it called linear time?
+// as data increases, time to run increases by the same amount.
+
+// anki card 7
+// O(2n + 6) can be said to be 2 O(n) and 6 O(1). is it O(n) or O(1)?
+// we can drop constants to make it O(n) on a high level.
+
+// anki card 8
+// demonstrate linear time with a function
+func printTheNames(theNames: [String]) {
+  for name in theNames {
+    print(name)
+  }
+}
+
+// keep working on the above cards before doing anything else.
