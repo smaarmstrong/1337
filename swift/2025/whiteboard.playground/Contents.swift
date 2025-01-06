30bf3da594ba455acad6d8d8d0ed599d2e2ab6d3
smaarmstrong <108431423+smaarmstrong@users.noreply.github.com>
2025-01-06 14:10:43 +0000

    demonstrate optimization

--- swift/2025/whiteboard.playground/Contents.swift
+++ swift/2025/whiteboard.playground/Contents.swift
@@ -1,1 +1,33 @@
 import Foundation
+
+func optimizedPrintSorted(_ array: [Int]) {
+  guard !array.isEmpty else { return }
+  
+  var currentCount: Int = 0
+  var minValue: Int = Int.min
+  
+  for value in array {
+    if value == minValue {
+      print(value)
+      currentCount += 1
+    }
+  }
+  
+  while currentCount < array.count {
+    var currentValue = array.max()!
+    for value in array {
+      if value < currentValue, value > minValue {
+        currentValue = value
+      }
+    }
+    
+    for value in array {
+      if value == currentValue {
+        print(value)
+        currentCount += 1
+      }
+    }
+    
+    minValue = currentValue
+  }
+}
