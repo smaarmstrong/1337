e69ece39786cbe003d58c9e3b8d24d4689763d0c
Sean Armstrong <seanmaarmstrong@gmail.com>
2025-11-09 21:38:02 +0000

    hello world

---
+++ rust/1.2/projects/hello_world/main.rs
@@ -0,0 +1,6 @@
+// a function named main; main is the first code that runs in every executable Rust program
+fn main() {
+    println!("Hello, world!"); // ! indicates a macro not a function
+}
+// compile with `rustc main.rs`
+// execute with `./main`
