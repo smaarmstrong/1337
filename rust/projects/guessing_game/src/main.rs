1a7f7dbd09d1ac3ded3959abded37f02b619d2fe
smaarmstrong <seanmaarmstrong@gmail.com>
2025-11-16 22:10:47 +0000

    add notes on syntax for std's io

--- rust/projects/guessing_game/src/main.rs
+++ rust/projects/guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io; // use io library from standard library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // created a new mutable variable that is currently bound to a new, empty instance of a String.
+
+    io::stdin() // call stdin function from io module
+        .read_line(&mut guess) // pass mutable reference & guess as argument
+        .expect("Failed to read line"); // error handling to prevent crash
+
+    println!("You guessed: {guess}");
 }
